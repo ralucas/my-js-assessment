2f81ddff11201ef7a4ae4585d0753c348c27ea99
R.A. Lucas <richard@richardalucas.com>
2015-03-29 14:52:04 -0600

    working

--- app/async.js
+++ app/async.js
@@ -3,15 +3,27 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-        
+      var defer = $.Deferred();
+      defer.resolve(value);
+      return defer.promise();  
     },
 
     manipulateRemoteData : function(url) {
-        console.log($);
-        console.log('hello');
-        $.get(url, function(data) {
-            console.log(data);
-        });
+      var defer = $.Deferred();
+      $.get(url).then(function(data) {
+        var people = data.people;
+        var output = [];
+        for (var i = 0; i < people.length; i++) {
+          output.unshift(people[i]['name']);
+        }
+        var matt = output.pop();
+        output.splice(2, 0, matt);
+        defer.resolve(output);
+      }).fail(function(err) {
+        defer.reject(err);
+      });
+
+      return defer.promise();
     }
   };
 });
--- app/recursion.js
+++ app/recursion.js
@@ -5,28 +5,47 @@ define(function() {
     listFiles: function(data, dirName) {
       console.log('d', data);
       var output = [];
-      if (!dirName) dirName = data.dir;
-      function each(files, dir) {
+      var f = data.files || [];
+      var bl;
+
+      if (!dirName) {
+        dirName = data.dir;
+        bl = true;
+      }
+
+      function each(files, dirN, bool) {
         for (var i = 0; i < files.length; i++) {
           if (typeof files[i] === 'object') {
-            if (dir === files[i]['dir']) {
-              output = [];
-              each(files[i]['files']);
+            //this caches the directory object
+            var newDir = files[i];
+            var newDirName = newDir.dir;
+            console.log('hi1', newDirName, dirN);
+            if (newDirName === dirN || bool) {
+              console.log(bool);
+              each(newDir.files, newDirName, true);
             } else {
-              each(files[i]['files']);
+              each(newDir.files, newDirName, false);
             }
-          } else {
+          } else if (bool && (typeof files[i] !== 'object') ) {
             output.push(files[i]);
           }
         }
         return output;
       }
-      var result = each(data.files, dirName);
+      var result = each(f, dirName, bl);
       return result;
+
     },
 
     permute: function(arr) {
-
+      var result = 1;
+      for (var i = 0; i < arr.length; i++) {
+        result *= arr[i];
+        console.log('r', result);
+      }
+      console.log('r2', result);
+      return result;
     }
+
   };
 });
